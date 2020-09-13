a2adafe682ac4036defe6da99d01abb119b7a31e
masterful-principle-0059 <william.neal83@gmail.com>
2020-09-13 14:58:53 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -185,3 +185,18 @@ def player_stats(player)
     end
   end
 end
+
+def big_shoe_rebounds
+  foot = 0
+  boards = 0
+  game_hash.each do |home_away, team_info|
+    team_info[:players].each do |player|
+      size = player[:shoe]
+      if size > foot
+        foot = size
+        boards = player[:rebounds]
+      end
+    end
+  end
+  boards
+end
