c7ee5d6f0ae5f4e7f758295feb3b1a8356633ce8
masterful-principle-0059 <william.neal83@gmail.com>
2020-09-13 12:19:45 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -171,7 +171,7 @@ end
 def player_numbers(team_name)
   array = []
    game_hash.each do |location, team_data|
-    if team_data[:team_name] == name
+    if team_data[:team_name] == team_name
       team_data[:players].each { |player_name, value|
         array << value[:number]
       end
